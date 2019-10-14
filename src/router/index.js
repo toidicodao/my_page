2b94481c9e57fb024bdf707714ab82e3112b4faa
Hoang Van Quan <quanhv@rikkeisoft.com>
2019-10-14 11:16:10 +0700

    edit

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import ViewHome from '@/views/ViewHome.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: ViewHome
     }
   ]
 })
---
+++ src/views/ViewHome.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="HomeView">
+        <h1>Welcome to Hoàng Quân</h1>
+    </div>
+</template>
+<script>
+
+export default {
+    name: "HomeView",
+}
+</script>
+
+<style>
+    h1{
+        text-align: center;
+        color: violet;
+        font-size: 30px;
+    }
+</style>>
+    
